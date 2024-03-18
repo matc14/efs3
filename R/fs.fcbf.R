f18ecc01cca8c02449d467f75362d04ce99baf39
matc14 <100838929+matc14@users.noreply.github.com>
2024-03-18 02:08:29 +0100

    Update fs.fcbf.R

--- R/fs.fcbf.R
+++ R/fs.fcbf.R
@@ -40,5 +40,6 @@ fs.fcbf <- function(x, y, params = list(su = 0.25)){
   x[row_num, col_name]
 })
   var.imp <- data.frame(name = name_content, score = df_output$SU)
+  print(var.imp)
   return(var.imp)
 }
